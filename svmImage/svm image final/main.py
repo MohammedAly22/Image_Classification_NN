349e240b49a54df56052d0f84ee53b65a84c966f
Mohammed Aly <90681796+MohammedAly22@users.noreply.github.com>
2022-01-01 08:28:27 +0200

    Update main.py

--- svmImage/svm image final/main.py
+++ svmImage/svm image final/main.py
@@ -27,14 +27,6 @@ for category in categories:
         except Exception as e:
             pass
 
-pick_in = open('data1.pickle', 'wb')
-pickle.dump(data, pick_in)
-pick_in.close()
-
-pick_in = open('data1.pickle', 'rb')
-data = pickle.load(pick_in)
-pick_in.close()
-
 random.shuffle(data)
 features = []
 labels = []
